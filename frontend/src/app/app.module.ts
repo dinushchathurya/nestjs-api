25315ee0b7351e76571180ccf0edab0203c8a07a
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-05 19:31:08 +0530

    feat: import contact component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatTableModule } from '@angular/material';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
