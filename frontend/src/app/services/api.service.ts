99925da134170bcecc3180f949d08a4a8afecb47
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-05 18:04:33 +0530

    feat: implement methods for CRUD operation

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -11,4 +11,20 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  public readContacts() {
+    return this.httpClient.get<Contact[]>(`${this.API_SERVER}/contacts`);
+  }
+
+  public createContact(contact: Contact) {
+    return this.httpClient.post<Contact>(`${this.API_SERVER}/contacts/create`, contact);
+  }
+
+  public updateContact(contact: Contact) {
+    return this.httpClient.put<Contact>(`${this.API_SERVER}/contacts/${contact.id}/update`, contact);
+  }
+
+  public deleteContact(id: number) {
+    return this.httpClient.delete(`${this.API_SERVER}/contacts/${id}/delete`);
+  }
+
 }
