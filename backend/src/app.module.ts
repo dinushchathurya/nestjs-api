9710afc6ec3074bd69c1105df45a08bcbb7575e1
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-05 14:51:01 +0530

    feat: import contact entity to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './entities/contact.entity';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: '',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-    })
+    }),
+    TypeOrmModule.forFeature([Contact]),
   ],
   controllers: [AppController],
   providers: [AppService],
