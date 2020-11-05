6b99fa18940bd5cecbf49bf99638f421b63b46b8
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-05 14:44:40 +0530

    feat: finish TypeORM configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/TypeOrmModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'nestng',
+      username: 'root',
+      password: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
